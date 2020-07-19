c3d04b724f3a5f3c2dfc63cebb245a483d13995d
Hyung1Jung <junghyungile@gmail.com>
2020-07-19 20:19:19 +0900

    feat : 익명함수

---
+++ basic/src/me/hyungil/no_name_function/Main.kt
@@ -0,0 +1,18 @@
+package me.hyungil.operator_overloading
+
+fun main() {
+    testFunction1()
+
+    // val testFunction2 = testFunction1
+    
+    val testFunction2 = fun(){
+        println("testFunction2 입니다")
+    }
+
+    testFunction2()
+    
+}
+
+fun testFunction1() {
+    println("testFunction1 입니다")
+}
