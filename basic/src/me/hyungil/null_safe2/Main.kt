4f657c48e2cd4229f09792feb5446a881dc05858
Hyung1Jung <junghyungile@gmail.com>
2020-08-12 20:19:19 +0900

    feat : null 안정성을 위한 형 변환

---
+++ basic/src/me/hyungil/null_safe2/Main.kt
@@ -0,0 +1,39 @@
+package me.hyungil.null_safe2
+
+fun main() {
+
+    testMethod1("안녕하세요")
+    testMethod1(null)
+
+    println("---------------------")
+
+    testMethod2("안녕하세요")
+    testMethod2(null)
+}
+
+fun testMethod1(str:String?){
+    println(str?.length)
+
+    if(str is String){
+        // 스마트 캐스팅 발생
+        println(str.length)
+    }
+
+    if(str != null){
+        // 스마트 캐스팅 발생생
+       println(str.length)
+    }
+
+    // println(str.length)
+}
+
+fun testMethod2(str:Any?){
+
+    if(str is String){
+        println(str.length)
+    }
+
+//    if(str != null){
+//        println(str.length)
+//    }
+}
