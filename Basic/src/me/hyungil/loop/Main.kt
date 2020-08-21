ab5cef11a7dceb9c521fe9c9b64946469d1061d1
Hyung1Jung <junghyungile@gmail.com>
2020-08-21 20:19:19 +0900

    feat : Loop

---
+++ Basic/src/me/hyungil/loop/Main.kt
@@ -0,0 +1,67 @@
+package me.hyungil.loop
+
+fun main() {
+    val a1 = 1..10
+
+    for(item in a1){
+        println("a1 : $item")
+    }
+
+    println("--------------------------")
+
+    val a2 = 1..10 step 2
+    for(item in a2){
+        println("a2 : $item")
+    }
+
+    println("-------------------------")
+
+    // val a3 = 10..1
+    val a3 = 10 downTo 1
+
+    for(item in a3){
+        println("a3 : $item")
+    }
+
+    println("----------------------------")
+
+    val a4 = 10 downTo 1 step 2
+
+    for(item in a4){
+        println("a4 : $item")
+    }
+
+    println("------------------------------")
+
+    var a5 = 0
+    // var a5 = 100
+
+    while(a5 < 10){
+        println("a5 : $a5")
+        a5++
+    }
+
+    println("--------------------------")
+
+    var a6 = 0
+    // var a6 = 100
+
+    do {
+        println("a6 : $a6")
+        a6++
+     }while(a6 < 10)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
