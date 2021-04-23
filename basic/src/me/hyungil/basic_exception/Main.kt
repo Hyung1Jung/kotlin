30c6b269d2ef6e262170f1f577d4fd5c7c6171fd
Hyung1Jung <junghyungile@gmail.com>
2021-04-24 01:57:32 +0900

    feat : 예외처리

---
+++ basic/src/me/hyungil/basic_exception/Main.kt
@@ -0,0 +1,37 @@
+package me.hyungil.basic_exception
+
+import java.lang.ArithmeticException
+import java.lang.Exception
+
+fun main() {
+    try {
+        // val a1 = 10 / 0
+
+//        val str:String? = null
+//        println(str!!.length)
+
+        val str2 = "안녕하세요"
+        val a2:Int = str2.toInt()
+        println("a2 : $a2")
+    }catch(e:ArithmeticException){
+        println("수학적 오류가 발생하였습니다")
+    }catch(e:KotlinNullPointerException){
+        println("Null 오류가 발생하였습니다")
+    }catch(e:Exception){
+        println("예외 발생")
+        e.printStackTrace()
+    }
+
+    println("이 부분이 수행될까요?")
+}
+
+
+
+
+
+
+
+
+
+
+
